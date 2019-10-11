94777f654a7eea45a391e655b9b564f2c7b8a96d
Wilfried MONTAGNE <wilfired.montagne@epsi.fr>
2019-10-11 09:03:19 +0200

    Ajout des méthodes calculInteret() pour le compte epargne ainsi que versement() et retrait() pour le compte payant

--- SegaBank/src/bo/CompteEpargne.java
+++ SegaBank/src/bo/CompteEpargne.java
@@ -2,8 +2,18 @@ package bo;
 
 public class CompteEpargne extends Compte {
 
-    public CompteEpargne(int id, double solde) {
+    private double tauxInteret;
+
+    public CompteEpargne(int id, double solde, double tauxInteret) {
         super(id, solde);
+        this.tauxInteret = tauxInteret;
+    }
+
+    public void calculInteret(){
+        double interets = this.getSolde()/tauxInteret;
+        this.setSolde(this.getSolde() + interets);
+        System.out.println("La somme de " + interets + "€ a été ajoutée ce compte.");
+        System.out.println("Solde actuel: " + this.getSolde() + "€.");
     }
 
 }
--- SegaBank/src/bo/ComptePayant.java
+++ SegaBank/src/bo/ComptePayant.java
@@ -6,4 +6,15 @@ public class ComptePayant extends Compte {
         super(id, solde);
     }
 
+    @Override
+    public void versement(double montant) {
+        this.setSolde(this.getSolde() + montant - (montant * 5 / 100));
+        System.out.println("La somme de " + montant + "€ a été versée sur ce compte.");
+    }
+
+    @Override
+    public void retrait(double montant) {
+        this.setSolde(this.getSolde() - montant - (montant * 5 / 100));
+        System.out.println("La somme de " + montant + "€ a été débitée de ce compte.");
+    }
 }
